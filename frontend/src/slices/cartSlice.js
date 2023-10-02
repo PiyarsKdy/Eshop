40944f67adc3d923a2b11279976ef00d25672928
Piyars <piyarskakadiya@gmail.com>
2023-10-02 12:22:54 +0530

    till Login and shipping

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []};
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : 
+{cartItems: [], shippingAddresses: {}, paymentMethod: 'PayPal' };
 
 
 
@@ -25,9 +26,13 @@ const cartSlice = createSlice({
         removeFromCart: (state, action) => {
             state.cartItems = state.cartItems.filter(x => x._id !== action.payload);
             return updateCart(state);
+        },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddresses = action.payload;
+            return updateCart(state);
         }
     }
 });
 
-export const {addToCart, removeFromCart} = cartSlice.actions;
+export const {addToCart, removeFromCart, saveShippingAddress} = cartSlice.actions;
 export default cartSlice.reducer;
