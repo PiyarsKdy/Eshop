b1723eeae78526e0608b6b1ba75e09521da76e72
Piyars <piyarskakadiya@gmail.com>
2023-10-05 14:40:31 +0530

    Place order and Summary page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,8 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 import { Provider } from 'react-redux';
 import store from './store';
 import PrivateRoute from './components/privateRoute';
@@ -34,6 +36,9 @@ const router = createBrowserRouter(
       <Route path='' element={<PrivateRoute/>}>
         <Route path='/shipping' element={<ShippingScreen/>}/>
         <Route path='/payment' element={<PaymentScreen/>}/>
+        <Route path='/placeorder' element={<PlaceOrderScreen/>}/>
+        <Route path='/order/:id' element={<OrderScreen/>}/>
+
       </Route>
     </Route>
   )
